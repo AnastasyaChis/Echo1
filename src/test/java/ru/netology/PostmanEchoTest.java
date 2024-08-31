2cbb1da9e3d5df14e87494e627deecafd1e1f1a6
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-08-31 17:32:29 +0300

    correct error commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -17,7 +17,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("Hi"))
+                .body("data", org.hamcrest.Matchers.equalTo(text))
         ;
 
     }
